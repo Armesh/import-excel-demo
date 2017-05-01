7ce94b5de7a6d8afc71a5cefc0098b0e5300a8df
Armesh <armesh@netccentric.com>
2017-05-01 13:58:58 +0800

    Migrations and models for new tables that will store the data imported from excel sheet

---
+++ database/migrations/2017_05_01_031955_create_addresses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->increments('client_id');
+            $table->string('address');
+
+            $table->foreign('client_id')
+              ->references('id')->on('clients')
+              ->onUpdate('cascade')
+              ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
